b6a6d9babb02f53e42a6c9c630252446e1f21a0e
Vladislav Bakin <vladislav@bakin.me>
2020-04-30 04:20:02 +0300

    add script template

---
+++ script_template.js
@@ -0,0 +1,17 @@
+/**
+ describe your script there
+
+ matches: <match expression>
+ matches: <match expression>
+
+ exclude_matches: <exclude_matches>
+ exclude_matches: <exclude_matches>
+
+ run_at: <document_idle|document_start|document_end>
+ all_frames: <true|false>
+ */
+
+// write your javascript there
+
+// if you want to add some stylesheets
+// add <script_name>.css file into scripts directory
